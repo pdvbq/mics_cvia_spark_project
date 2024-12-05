01f1b35add8e9f9971db0e430ccdc41caed3dd1b
Pedro Soares <pmbs.123@gmail.com>
2024-12-05 15:09:34 +0100

    chore: improve settings format

--- spark/cli/fetch.py
+++ spark/cli/fetch.py
@@ -8,12 +8,12 @@ app = typer.Typer()
 @app.command()
 def stream1():
     tools.download_dataset(
-        Settings.stream1_url, "stream1", Settings.data_root_dir, True
+        Settings.fetch_cfg.stream1_url, "stream1", Settings.fetch_cfg.data_dir, True
     )
 
 
 @app.command()
 def stream2():
     tools.download_dataset(
-        Settings.stream2_url, "stream2", Settings.data_root_dir, True
+        Settings.fetch_cfg.stream2_url, "stream2", Settings.fetch_cfg.data_dir, True
     )
--- spark/settings.py
+++ spark/settings.py
@@ -2,12 +2,9 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Settings:
-    stream1_url = "https://uniluxembourg-my.sharepoint.com/:u:/g/personal/pedro_bastos_uni_lu/EfXBeG7PFypEs5sM3NzMB0MByiEaKFaoD8WKfzQN_6UPog?download=1"
-    stream2_url = ""
-    data_root_dir = "data"
-    stream1_dir = "stream1"
-    stream2_dir = "stream2"
+class DatasetCfg:
+    image_width = 1024
+    image_height = 1024
     class_map = {
         "proba_2": 0,
         "cheops": 1,
@@ -21,3 +18,18 @@ class Settings:
         "soho": 9,
         "xmm_newton": 10,
     }
+
+
+@dataclass
+class FetchDatasetCfg:
+    data_dir = "data"
+    stream1_url = "https://uniluxembourg-my.sharepoint.com/:u:/g/personal/pedro_bastos_uni_lu/EfXBeG7PFypEs5sM3NzMB0MByiEaKFaoD8WKfzQN_6UPog?download=1"
+    stream2_url = ""
+    stream1_dir = "stream1"
+    stream2_dir = "stream2"
+
+
+@dataclass
+class Settings:
+    fetch_cfg = FetchDatasetCfg()
+    dataset_cfg = DatasetCfg()
