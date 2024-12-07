4d47b6c4814674fda4d8c205be4f541031a12d4a
Pedro Soares <pmbs.123@gmail.com>
2024-12-07 18:43:51 +0100

    feat: add test function

--- spark/pipelines/yolo.py
+++ spark/pipelines/yolo.py
@@ -1,6 +1,8 @@
 from coloredlogs import logging
+import cv2
 from ultralytics import YOLO
 from spark.pipelines.pipeline import Pipeline
+import csv
 
 logger = logging.getLogger(__name__)
 
@@ -12,15 +14,25 @@ class YoloPipeline(Pipeline):
     def train(self, **kwargs):
         epochs = kwargs["train"]["epochs"]
         data = kwargs["dataset_metadata"]
-        save_file = kwargs["train"]["save_file"]
+        save_file = kwargs["train"].get("save_file", "")
+        batch = kwargs["train"]["batch"]
 
-        self.model.train(data=data, epochs=epochs)
+        self.model.train(data=data, epochs=epochs, batch=batch)
 
         if save_file != "":
             self.model.save(save_file)
 
     def test(self, **kwargs):
-        logger.warning("Test is not yet implemented")
+        source = kwargs["test"]["source"]
+        results = self.model.predict(source=source, stream=True)
+        file = kwargs["test"]["output"]
+
+        # TODO: Currently, this is saving as a yolo format.
+        # The advised format is image_name, class, bbox
+
+        for result in results:
+            # This saves as class_id bbox (using yolo format)
+            result.save_txt(file)
 
     def validate(self, **kwargs):
         data = kwargs["dataset_metadata"]
