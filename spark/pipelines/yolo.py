442206f3169ab91e3789c41532f6cf7424c38c82
Pedro Soares <pmbs.123@gmail.com>
2024-12-16 16:32:10 +0100

    fix merge

--- spark/pipelines/yolo.py
+++ spark/pipelines/yolo.py
@@ -55,9 +55,12 @@ class YoloPipeline(Pipeline):
             writer.writerow(["filename", "class", "bbox"])
             for result in track(results, description="Predicting...", total=total_imgs):
                 filename = os.path.basename(result.path)
+                filename = f"{filename.split('.')[0]}.png"
+                if result.boxes is None:
+                    writer.writerow([filename, "", ""])
+                    continue
                 # INFO: Trick in order to change extension type back to .png
                 # remove this in case it's fixed in codalab
-                filename = f"{filename.split('.')[0]}.png"
                 for box in result.boxes:
                     cls = result.names[box.cls.tolist()[0]]
                     xyxy = list(
