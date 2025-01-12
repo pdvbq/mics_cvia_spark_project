308f9468a048fe6e16692573a8a6cba9b71657ac
pdvbq <147347056+pdvbq@users.noreply.github.com>
2025-01-12 22:24:32 +0100

    fix: pipeline improvements for maskrcnn

--- spark/pipelines/maskrcnn.py
+++ spark/pipelines/maskrcnn.py
@@ -6,38 +6,63 @@ from torchvision.models.detection import maskrcnn_resnet50_fpn_v2
 from spark.pipelines.pipeline import Pipeline
 from spark.settings import DetectionDatasetCfg
 from spark.utils.dataset import SparkDataset
+from spark.utils.transforms import ImgTransform
+from spark.utils.mask import generate_masks
 
 
 class MaskRCNNPipeline(Pipeline):
-    def __init__(self):
+    def _optim_select(self, config: dict):
+        """
+        Select optimiser based on config
+        """
+        option_dict = {"SGD": optim.SGD, "Adam": optim.Adam, "AdamW": optim.AdamW}
+        assert (
+            config["train"]["optimizer"] in option_dict.keys()
+        ), f"Invalid optimiser: {config['train']['optimizer']}"
+        return option_dict[config["train"]["optimizer"]]
+
+    def __init__(self, config):
+        self.config = config
+        BATCH_SIZE = config["train"]["batch"]
+        
         self.device = "cuda:0" if torch.cuda.is_available else "cpu"
-        self.model = maskrcnn_resnet50_fpn_v2(num_classes=11)
-        self.optim = optim.AdamW(self.model.parameters(), lr=0.005)
-        self.num_epochs = 5
+        self.model = maskrcnn_resnet50_fpn_v2(num_classes=11).to(self.device)
+        self.optim = self._optim_select(config)(
+            self.model.parameters(), lr=config["train"]["lr0"]
+        )
+        self.num_epochs = config["train"]["epochs"]
         self.train_ds = SparkDataset(
-            DetectionDatasetCfg().class_map, root_dir="../datasets/stream1", split="train"
+            DetectionDatasetCfg().class_map,
+            root_dir="../datasets/stream1",
+            split="train",
+            transform=ImgTransform(config["input_size"]),
         )
         self.val_ds = SparkDataset(
-            DetectionDatasetCfg().class_map, root_dir="../datasets/stream1", split="validation"
+            DetectionDatasetCfg().class_map,
+            root_dir="../datasets/stream1",
+            split="validation",
+            transform=ImgTransform(config["input_size"]),
         )
         # self.test_ds = SparkDataset(
         #     DetectionDatasetCfg().class_map, root_dir="data/stream1", split="test"
         # )
-        self.train_dl = DataLoader(self.train_ds, batch_size=64, shuffle=True)
-        self.val_dl = DataLoader(self.val_ds, batch_size=64, shuffle=True)
+        self.train_dl = DataLoader(self.train_ds, batch_size=BATCH_SIZE, shuffle=True)
+        self.val_dl = DataLoader(self.val_ds, batch_size=BATCH_SIZE, shuffle=True)
         # self.test_dl = DataLoader(self.test_ds, batch_size=64, shuffle=True)
-        # self.
 
     def train(self, **kwargs):
-        # TODO:
-        # requires train loop logic in model definition
         self.model.train()
         for i in range(self.num_epochs):
-            for img_batch, label_batch in self.train_dl:
+            for img_batch, label_batch, bbox_batch in self.train_dl:
                 self.optim.zero_grad()
                 images = list(img_batch.to(self.device))
                 labels = list(label_batch.to(self.device))
-                loss_dict = self.model(images, labels)
+                bboxes = list(bbox_batch.to(self.device))
+                targets = [
+                    {"labels": lbl, "boxes": bbox.unsqueeze(0), "masks": generate_masks(bbox.unsqueeze(0), self.config["input_size"])}
+                    for lbl, bbox in zip(labels, bboxes)
+                ]
+                loss_dict = self.model(images, targets=targets)
                 losses = sum(loss for loss in loss_dict.values())
                 losses.backward()
                 self.optim.step()
