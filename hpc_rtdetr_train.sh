9af995500da5f88659d075403fe2b7c761cbce02
Cosmin Rădoi cosmin.radoi.001@student.uni.lu <cradoi@access1.iris-cluster.uni.lux>
2025-01-14 21:46:31 +0100

    rtdetr giving me trouble

---
+++ hpc_rtdetr_train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+### Request one GPU tasks for 4 hours - dedicate 1/4 of available cores for its management
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 14
+#SBATCH -G 1
+#SBATCH --time=6:00:00
+#SBATCH --partition gpu
+#SBATCH --qos normal
+
+
+print_error_and_exit() { echo "***ERROR*** $*"; exit 1; }
+module purge || print_error_and_exit "No 'module' command"
+
+nvidia-smi
+
+uv run main.py pipeline rtdetr train
