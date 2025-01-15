1b33c8ba485f1e3c9e04dbdab48d2a8153603138
pdvbq <147347056+pdvbq@users.noreply.github.com>
2025-01-15 13:32:19 +0100

    feat: SAM mask generation

--- spark/utils/mask.py
+++ spark/utils/mask.py
@@ -1,5 +1,9 @@
+import os
+from glob import glob
 import numpy as np
 import torch
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+import cv2
 
 def generate_masks(boxes, image_size):
     """
@@ -18,3 +22,28 @@ def generate_masks(boxes, image_size):
         mask[y_min:y_max, x_min:x_max] = 1
         masks.append(mask)
     return torch.tensor(masks, dtype=torch.uint8)
+
+def generate_sam_masks(img_path):
+    # pip install git+https://github.com/facebookresearch/segment-anything.git
+    # download the checkpoint from the repo
+    sam = sam_model_registry["vit_b"](checkpoint="sam_vit_b.pth")
+    sam.to("cuda")
+    sam.eval()
+    mask_generator = SamAutomaticMaskGenerator(sam)
+    partition = "/mnt/lscratch/users/pchernakov"
+    mask_path = img_path.replace("images", "masks").replace("../", "")
+    os.makedirs(mask_path, exist_ok=True)
+    for img_file in glob(os.path.join(img_path, "*.jpg")):
+        basename = os.path.basename(img_file)
+        dest = os.path.join(partition, mask_path, basename.replace(".jpg", ".npy"))
+        if os.path.exists(dest):
+            continue
+        img = cv2.imread(img_file)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        masks = mask_generator.generate(img)
+        stacked_masks = np.stack([mask["segmentation"] for mask in masks], axis=0)
+        np.save(dest, stacked_masks)
+        # cv2.imwrite(img.replace(".jpg", "_mask.jpg"), mask)
+
+if __name__ == "__main__":
+    generate_sam_masks("../datasets/stream1/images/train")
